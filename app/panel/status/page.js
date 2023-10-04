28743a6041dbd17e6ba50bea88dcbbb378c13b69
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-10-04 18:25:43 +0300

    changed the imported logo name to camel case

--- app/panel/status/page.js
+++ app/panel/status/page.js
@@ -3,13 +3,13 @@ import Image from 'next/image'
 import { cookies } from 'next/headers'
 
 import StatusPage from 'components/StatusPage';
-import Logo from '/images/logo-white.png';
+import logo from '/images/logo-white.png';
 
 export default function Page() {
   const cookie = cookies().get('user')?.value
   return (
     <div className='bg-gradient-to-b from-black/75 via-transparent to-fuchsia-700/75 h-screen grid'>
-      <Image src={Logo} alt='logo' className='absolute top-0 left-0 pl-4 w-16 md:w-24'/>
+      <Image src={logo} alt='logo' className='absolute top-0 left-0 pl-4 w-16 md:w-24'/>
       <StatusPage initial={cookie} />  
     </div>
   )
