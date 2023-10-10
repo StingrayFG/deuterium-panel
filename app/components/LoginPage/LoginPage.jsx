dadca746c35fad3f9b617f748d522ac21974502d
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-10-10 16:42:55 +0300

    added animated message to LoginPage

--- app/components/LoginPage/LoginPage.jsx
+++ app/components/LoginPage/LoginPage.jsx
@@ -5,8 +5,20 @@ import axios from 'axios';
 import { useRouter } from 'next/navigation'
 
 export default function LoginPage() {
+
   const router = useRouter();
   const userData = sessionStorage.getItem('user')
+
+  const delay = ms => new Promise(res => setTimeout(res, ms));
+  
+  const [isFailed, setIsFailed] = useState();
+  const [showingMessage, setShowingMessage] = useState();
+
+  const showMessage = async () => {
+    setShowingMessage(true);
+    await delay(1500);
+    setShowingMessage(false);
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -17,46 +29,55 @@ export default function LoginPage() {
         if (res.data.exists === true) {
           sessionStorage.setItem('user', JSON.stringify({login: event.target.elements.login.value, accessToken: res.data.accessToken}));
           router.replace('/panel/status');
+        } else {
+          showMessage();
         }
       })
       .catch(err => {
-        setIsFailed(true);
+        showMessage();
+        setIsFailed(true)
         console.error(err);
       });
   };
-
+  
   if (!userData) {
     return (
-      <div className='w-96 h-60 mb-12 place-self-center grid
-      bg-neutral-900/50
-      border-dashed border-2 border-fuchsia-200 rounded-lg'>
-        <form onSubmit={handleSubmit} className='grid place-self-center w-full
-        border-dashed border-2 border-transparent rounded-lg'>
-          <input className='h-12 w-10/12 pl-2 place-self-center
-          bg-neutral-700 font-sans text-xl text-neutral-200
-          border-solid border-2 border-neutral-200 rounded-md outline-none '
-            placeholder='Login'
-            name='login'
-            type='text'/>
-          <input className='h-12 w-10/12 pl-2 mt-2 place-self-center
-          bg-neutral-700 font-sans text-xl text-neutral-200
-          border-solid border-2 border-neutral-200 rounded-md outline-none '
-            placeholder='Password'
-            name='password'
-            type='password'/>    
-          <button className='h-12 w-10/12 pl-2 mt-6 grid place-self-center
-          bg-neutral-900 hover:bg-neutral-800/75 active:bg-neutral-700/75 font-sans text-xl text-neutral-200 
-          border-solid border-2 border-fuchsia-900 rounded-md outline-none '>
-            <p className='place-self-center font-sans text-xl text-neutral-200'>
-              Sign in
-            </p>
-          </button >
-        </form>              
-      </div>
+      <div className='w-96 h-72 place-self-center grid 
+      text-2xl font-sans text-neutral-200'>
+        <div className='w-96 h-[14rem] place-self-center grid
+        bg-neutral-900/50
+        border-dashed border-2 border-fuchsia-200 rounded-lg'>
+          <form onSubmit={handleSubmit} className='grid place-self-center w-full
+          border-dashed border-2 border-transparent rounded-lg'>
+            <input className='h-10 w-10/12 pl-2 place-self-center
+            bg-neutral-700 text-xl
+            border-solid border-2 border-neutral-200 rounded-md outline-none '
+              placeholder='Login'
+              name='login'
+              type='text'/>
+            <input className='h-10 w-10/12 pl-2 mt-2 place-self-center
+            bg-neutral-700 text-xl
+            border-solid border-2 border-neutral-200 rounded-md outline-none '
+              placeholder='Password'
+              name='password'
+              type='password'/>    
+            <button className='h-10 w-10/12 pl-2 mt-6 grid place-self-center
+            bg-neutral-900 hover:bg-neutral-800/75 active:bg-neutral-700/75 font-sans text-xl text-neutral-200 
+            border-solid border-2 border-fuchsia-900 rounded-md outline-none '>
+              <p className='place-self-center text-xl'>
+                Sign in
+              </p>
+            </button >
+          </form>              
+        </div>
+        <p className={`place-self-center mt-0 text-xl transition-all duration-250
+        ${showingMessage ? 'opacity-100': 'opacity-0'}`}>
+          {isFailed ? 'Something went wrong' : 'Wrong user data'}
+        </p>       
+      </div>  
     ) 
   } else {
     router.replace('/panel/status');
   }
-  
-  
 }
+
